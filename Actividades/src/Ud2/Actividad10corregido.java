66a6a15be50326f483fdee6bd14377950f276cdf
Adri-marq <marquez.lozano.adrian@iescamas.es>
2025-09-29 18:41:00 +0200

    ejercicios 10 corregido

--- Actividades/src/Ud2/Actividad10corregido.java
+++ Actividades/src/Ud2/Actividad10corregido.java
@@ -1,10 +1,44 @@
 package Ud2;
-
+import java.util.Scanner;
 public class Actividad10corregido {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int numero,unidades,decenas,centenas,millares;
+		System.out.println("Escribe un número entre 0 y 9.999:");
+		numero = sc.nextInt();
+		if (numero < 10) {
+			System.out.println("Error: no un numero de un solo digito no puede ser capicua");
+		}else {
+			if (numero < 100) {
+				unidades = numero % 10;
+				decenas = numero/10;
+				if (unidades == decenas) {
+					  System.out.println("El número es capicúa.");
+				} else {
+					  System.out.println("El número no es capicúa.");
+				}
+			}else if (numero < 1000) {
+				unidades = numero % 10;
+				centenas = numero/100;
+				if (unidades == centenas) {
+					  System.out.println("El número es capicúa.");
+				} else {
+					  System.out.println("El número no es capicúa.");
+				}
+			
+			}else if (numero < 1000) {
+				unidades = numero % 10;
+				millares= numero/1000;
+				if (unidades == millares) {
+					  System.out.println("El número es capicúa.");
+				} else {
+					  System.out.println("El número no es capicúa.");
+				}
+			}else {
+				System.out.println("Error: El numero no esta entre los valores designados");
+			}
+		}	
 	}
-
 }
