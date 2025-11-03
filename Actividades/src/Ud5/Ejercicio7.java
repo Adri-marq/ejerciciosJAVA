45971c041d1fb8e865630e1ae1dc08fe3ad61bc8
Adri-marq <marquez.lozano.adrian@iescamas.es>
2025-11-03 19:07:56 +0100

    Ejercicio 7 un poqutio

--- Actividades/src/Ud5/Ejercicio7.java
+++ Actividades/src/Ud5/Ejercicio7.java
@@ -1,5 +1,6 @@
 package Ud5;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ejercicio7 {
@@ -15,14 +16,30 @@ public class Ejercicio7 {
 		System.out.println("dime los numeros de la primera tabla");
 		for(int i=0;i<tabla1.length;i++) {
 			tab1=sc.nextInt();
-			tabla1[1]=tab1;
-		}
-		System.out.println("dime los numeros de la segunda tabla");
+			tabla1[i]=tab1;
+			}
+		Arrays.sort(tabla1);
+	
+				System.out.println("\ndime los numeros de la segunda tabla");
 		for(int i=0;i<tabla2.length;i++) {
 			tab2=sc.nextInt();
-			tabla2[1]=tab2;
-		}
+			tabla2[i]=tab2;
+			}
+		Arrays.sort(tabla2);
+		System.out.println("tablas:");
+		System.out.println("tabla1 ->"+Arrays.toString(tabla1));
+		System.out.println("tabla2 ->"+Arrays.toString(tabla2));
 		
+	for(int i=0;i<tablacom.length;i++) {
+		for(int j=0;j<tabla2.length;j++) {
+			if(tabla1[i]>tabla2[j]) {
+				tablacom[i]=tabla2[j];
+			}else {
+				tablacom[i]=tabla2[j];
+			}
+		}
+	}
+	System.out.println("tablacompuesta ->"+Arrays.toString(tablacom));
 	}
 
 }
