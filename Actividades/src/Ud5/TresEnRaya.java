183fb8271953a619779b3be8e86ecd85789258b6
Adri-marq <marquez.lozano.adrian@iescamas.es>
2025-11-13 20:08:07 +0100

    Tres en rayas

--- Actividades/src/Ud5/TresEnRaya.java
+++ Actividades/src/Ud5/TresEnRaya.java
@@ -3,16 +3,25 @@ package Ud5;
 import java.util.Scanner;
 
 public class TresEnRaya {
-
+	public static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		int opcion=0;
-		do { mostrarMenu(); 
+		
+		int opcion=0,turno=0;
+		char [][] tablero = new char [3][3];
+		for(int i=0; i<tablero.length;i++) {
+			for(int j=0;j<tablero[i].length;j++) {
+				tablero [i][j]=' ';
+			}
+		}
+		do {
+			mostrarTablero(tablero);
+		mostrarMenu(); 
 		opcion = sc.nextInt(); 
 		switch(opcion){
 		case 1:
-			jugar(sc);
+			jugar(tablero,turno);
+			turno++;
 			break;
 		case 2:
 			mostrarInstrucciones();
@@ -20,15 +29,17 @@ public class TresEnRaya {
 		case 3:
 			System.out.println("adios");
 			break;
-		} 
-		} while(opcion != 3);
+		default:
+			System.out.println("No hay ninguna opcion con ese numero");
+		}
+		} while(opcion != 4);
 	}
 	
 	public static void mostrarMenu() {
 		System.out.println("que quieres hacer:");
 		System.out.println("1) jugar" );
 		System.out.println("2) ver instrucciones");
-		System.out.println("salir");
+		System.out.println("3) salir");
 	}
 	public static void mostrarInstrucciones() {
 		System.out.println("\nInstrucciones del juego:");
@@ -37,34 +48,47 @@ public class TresEnRaya {
         System.out.println("3. El primer jugador en alinear 3 símbolos de forma horizontal, vertical o diagonal gana.");
         System.out.println("4. ¡Buena suerte!");
 	}
-	public static void jugar() {
-		char jugador ='x';
-	}
-	public static void inicializarTablero() {
-		char [][] tablero= new char[3][3];
-		for(int i=0;i<tablero.length;i++) {
-			for(int j=0;j<tablero[i].length;j++) {
-				tablero[i][j]=' ';
-			}	
+	public static void jugar(char[][] tablero,int turno) {
+		char celda=' ', jugador;
+		int Py,Px;
+		if (turno % 2 == 0) {
+            jugador='X';
+            System.out.println("Turno del jugador X:");
+        }else {
+        	jugador='O';
+            System.out.println("Turno del jugador O:");
+        
+        }
+		System.out.println("dime la celda a la cual quieres escribir");
+		System.out.println("fila:");
+		Py= sc.nextInt();
+		System.out.println("columna:");
+		Px= sc.nextInt();
+		if(tablero[Py][Px] !=' ') {
+			System.out.println("En esa posicion ya has puesto " +tablero[Py][Px]);
 		}
+		while(tablero[Py][Px] ==' '|(jugador == 'x'|jugador == 'o')) {
+		tablero[Py][Px]=jugador;
+		}
+		if(tablero[Py][Px] ==' ') {
+			System.out.println("esa posicion ya esta escrito");
+		}
+		turno++;
 	}
-	public static void mostrarTablero() {
-		char [][] tablero= new char[3][3];
-        for (int i = 0; i < 3; i++) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-        
-        
-        for (int i = 0; i < 3; i++) {
-            System.out.print(i + " ");
-            for (int j = 0; j < 3; j++) {
-                System.out.print(tablero[i][j]); 
-                if (j < 2) System.out.print(" | "); 
-            }
-            System.out.println();
-            if (i < 2) System.out.println("  ---+---+---"); // 
-        }
+	public static void mostrarTablero(char[][] tablero) {
+		 System.out.println("   0   1   2 ");
+		 System.out.println("  ---+---+---");
+	        for (int i = 0; i < tablero.length; i++) {
+	            System.out.print(i + "│");
+	            for (int j = 0; j < tablero[i].length; j++) {
+	                System.out.print(" " + tablero[i][j] + " │");
+	            }
+	            System.out.println();
+	            if (i < tablero.length - 1) {
+	                System.out.println("  ---+---+---");
+	            }
+	        }
+	        System.out.println("  ---+---+---");
 	}
 	public static void hayGanador() {
 	
