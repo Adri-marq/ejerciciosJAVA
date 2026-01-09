b3ba73ba44920c417e83a782d50a4d0b8faabad6
Adri-marq <marquez.lozano.adrian@iescamas.es>
2026-01-09 18:43:10 +0100

    calculadora

---
+++ Ejemplos/dni/src/main/java/es/calculadora/main.java
@@ -0,0 +1,15 @@
+package es.calculadora;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ControladorCalculadora calcu = new ControladorCalculadora(7,0);
+	
+	double div = calcu.dividir();
+	System.out.println(div);
+	
+	
+	}
+
+}
