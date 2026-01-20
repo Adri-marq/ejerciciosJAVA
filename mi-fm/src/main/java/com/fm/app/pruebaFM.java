4dba6e851a7d97b3b79c750dbd1bc8a44dd12d39
Adri-marq <marquez.lozano.adrian@iescamas.es>
2026-01-20 21:10:14 +0100

    sintonizador de emisoras fm 2

---
+++ mi-fm/src/main/java/com/fm/app/pruebaFM.java
@@ -0,0 +1,19 @@
+package com.fm.app;
+
+public class pruebaFM {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Sintonizador fm = new Sintonizador();
+	
+	fm.display();
+	fm.down();
+	fm.display();
+	fm.up();
+	fm.display();
+	fm.down();
+	fm.display();
+	}
+
+}
